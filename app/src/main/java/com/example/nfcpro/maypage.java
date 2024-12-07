8d75ab15bb1dda7ca3fbef6ce6400589e11fb911
ko1927 <132965688+ko1927@users.noreply.github.com>
2024-12-07 22:47:00 +0900

    마이페이지

--- app/src/main/java/com/example/nfcpro/maypage.java
+++ app/src/main/java/com/example/nfcpro/maypage.java
@@ -1,64 +1,99 @@
 package com.example.nfcpro;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link maypage#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class maypage extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView boothNameText;
+    private TextView boothCodeText;
+    private TextView boothLocationText;
+    private DatabaseReference databaseReference;
+    private static final String DATABASE_URL = "https://nfctogo-f4da1-default-rtdb.firebaseio.com/";
 
     public maypage() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment maypage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static maypage newInstance(String param1, String param2) {
-        maypage fragment = new maypage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Initialize Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance(DATABASE_URL);
+        databaseReference = database.getReference("nfcpro");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maypage, container, false);
+        View view = inflater.inflate(R.layout.fragment_maypage, container, false);
+
+        // Initialize TextViews
+        boothNameText = view.findViewById(R.id.boothNameText);
+        boothCodeText = view.findViewById(R.id.boothCodeText);
+        boothLocationText = view.findViewById(R.id.boothLocationText);
+
+        // Load booth data
+        loadBoothData();
+
+        return view;
+    }
+
+    private void loadBoothData() {
+        // Get booth earnings data
+        databaseReference.child("rank").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Get booth info for the one with highest earnings
+                long highestEarnings = 0;
+                String topBoothId = "";
+
+                for (DataSnapshot boothSnapshot : dataSnapshot.getChildren()) {
+                    long earnings = boothSnapshot.getValue(Long.class);
+                    if (earnings > highestEarnings) {
+                        highestEarnings = earnings;
+                        topBoothId = boothSnapshot.getKey();
+                    }
+                }
+
+                final long finalEarnings = highestEarnings;
+                final String finalBoothId = topBoothId;
+
+                // Get detailed booth information
+                databaseReference.child("booths").child(topBoothId)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot boothSnapshot) {
+                                String boothName = boothSnapshot.child("name").getValue(String.class);
+
+                                // Update UI
+                                boothNameText.setText("부스 이름: " + boothName);
+                                boothCodeText.setText("부스 코드: " + finalBoothId);
+                                boothLocationText.setText("총 매출: ₩" + String.format("%,d", finalEarnings));
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                // Handle error
+                            }
+                        });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
     }
 }
